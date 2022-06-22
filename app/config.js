ceae1c22ddf41d6295247ef8758ea24ed52eb266
Chris Hill-Scott <me@quis.cc>
2022-06-22 15:08:14 +0100

    Add service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Get a passport in 1 week',
 
   // Default port that prototype runs on
   port: '3000',
